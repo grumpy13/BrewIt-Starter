ae47e18a92ee8ec74bbb1795151af07454df0e4c
Mariam Ali <mariamqali13@gmail.com>
2020-02-05 22:07:33 +0300

    done

--- src/App.js
+++ src/App.js
@@ -5,20 +5,27 @@ import "./App.css";
 
 //Data
 import allBrewMethods from "./data";
-
-//Components
 import AboutPage from "./Components/AboutPage";
 import NavBar from "./Components/NavBar";
 
+//Components
+import BrewingList from "./Components/BrewingList/";
+
 function App() {
+  const [brewingMethods] = useState(allBrewMethods);
   const [showHome, setShowHome] = useState(true);
 
   return (
     <>
       <NavBar setShowHome={setShowHome} />
       <div className="App my-5">
-        <div className="container">{showHome ? null : <AboutPage />}</div>
-
+        <div className="container">
+          {showHome ? (
+            <BrewingList brewingMethods={brewingMethods} />
+          ) : (
+            <AboutPage />
+          )}
+        </div>
       </div>
     </>
   );
